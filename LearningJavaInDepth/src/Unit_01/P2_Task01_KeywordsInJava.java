d9851c4342bdb7f7caa25c44f94ea78971150d02
dash2 <dash2@yash>
2022-03-31 14:41:04 +0530

    3rd commit

--- LearningJavaInDepth/src/Unit_01/P2_Task01_KeywordsInJava.java
+++ LearningJavaInDepth/src/Unit_01/P2_Task01_KeywordsInJava.java
@@ -37,4 +37,18 @@ class ABC{
 		System.out.println("In class Two.......");
 	}
 }
+
+/*Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+	Syntax error on token "double", invalid VariableDeclaratorId
+	Syntax error on token "new", invalid AnnotationName
+	Syntax error, insert "[ ]" to complete Dimension
+	Syntax error, insert "VariableDeclarators" to complete LocalVariableDeclaration
+	Syntax error, insert ";" to complete LocalVariableDeclarationStatement
+	Duplicate local variable $missing$
+	Void methods cannot return a value
+	ABC cannot be resolved to a variable
+	Syntax error on token "int", delete this token
+
+	at Unit_01.P2_Task01_KeywordsInJava.main(P2_Task01_KeywordsInJava.java:7)
+/*
 	
